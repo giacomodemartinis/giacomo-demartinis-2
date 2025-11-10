ce90c93c277a11fe558aac42946a4471b9be9c28
giacomodemartinis <giacomodemartinis1@gmail.com>
2025-11-10 20:11:46 +0100

    portfolio v2
    v2

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import styles from './ProjectsStyles.module.css';
-import viberr from '../../assets/viberr.png'
-import freshBurger from '../../assets/fresh-burger.png'
+import soldy from '../../assets/soldy.png'
+import walle from '../../assets/walle.png'
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -8,8 +8,8 @@ function Projects() {
     <section id="projects" className={styles.container}>
         <h1 className='sectionTitle'>Projects</h1>
         <div className={styles.projectsContainer}>
-            <ProjectCard src={viberr} link='https://github.com/giacomodemartinis/giacomo-demartinis-2' h3='Viberr' p='Streaming App'/>
-            <ProjectCard src={freshBurger} link='https://github.com/giacomodemartinis/giacomo-demartinis-2' h3='Fresh Burger' p='Hamburger Restaurant'/>
+            <ProjectCard src={soldy} link='https://github.com/giacomodemartinis/giacomo-demartinis-2' h3='Soldy' p='Budgeting App'/>
+            <ProjectCard src={walle} link='https://github.com/giacomodemartinis/giacomo-demartinis-2' h3='Walle' p='Wallpaper App'/>
         </div>
     </section>
   )  
