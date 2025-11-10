c905f44fe6e7e53d6dda38624b6e21015a841bb3
giacomodemartinis <giacomodemartinis1@gmail.com>
2025-11-10 23:43:36 +0100

    v4
    v4

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,6 +1,8 @@
 // src/data/projectsData.js
 import soldyGif from '../assets/soldy-demo.gif';
 import walleGif from '../assets/walle-demo.gif';
+import auriGif from '../assets/auri-demo.gif';
+import moodtrackerGif from '../assets/moodtracker-demo.gif'
 
 export const projects = [
   {
@@ -18,6 +20,24 @@ export const projects = [
     gif: walleGif,
     overview:
       'Walle is a budgeting app that helps users track expenses, visualize spending, and stay within monthly limits.',
+    techStack: ['React', 'Vite', 'CSS Modules'],
+    githubUrl: 'https://github.com/giacomodemartinis/giacomo-demartinis-2',
+  },
+  {
+    slug: 'auri',
+    name: 'Auri',
+    gif: auriGif,
+    overview:
+      'Auri is a budgeting app that helps users track expenses, visualize spending, and stay within monthly limits.',
+    techStack: ['React', 'Vite', 'CSS Modules'],
+    githubUrl: 'https://github.com/giacomodemartinis/giacomo-demartinis-2',
+  },
+  {
+    slug: 'moodtracker',
+    name: 'Moodtracker',
+    gif: moodtrackerGif,
+    overview:
+      'MoodTracker is a journaling app that helps users track expenses, visualize spending, and stay within monthly limits.',
     techStack: ['React', 'Vite', 'CSS Modules'],
     githubUrl: 'https://github.com/giacomodemartinis/giacomo-demartinis-2',
   },
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,16 +1,20 @@
 // src/sections/Projects/Projects.jsx
 import styles from './ProjectsStyles.module.css';
-import soldy from '../../assets/soldy.png';
-import walle from '../../assets/walle.png';
+import soldy from '../../assets/soldy15.png';
+import walle from '../../assets/walle2.png';
+import auri from '../../assets/auri.png'
+import moodtracker from '../../assets/moodtracker.png'
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-      <h1 className="sectionTitle">Projects</h1>
+      <h1 className="sectionTitle">Products</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard src={soldy} h3="Soldy" p="Budgeting App" />
         <ProjectCard src={walle} h3="Walle" p="Wallpaper App" />
+        <ProjectCard src={auri} h3="Auri" p="Flashcards App" />
+        <ProjectCard src={moodtracker} h3="Moodtracker" p="Journaling App" />
       </div>
     </section>
   );
